c3f050dd2812c19e4a2942d37d3ba05972f14818
Christian Roque <croqu008@fiu.edu>
2019-09-19 16:48:53 -0400

    finished models

--- GoodWatch/models/Comment.js
+++ GoodWatch/models/Comment.js
@@ -1,19 +1,11 @@
-// Comments:
-
-// authorofComment:
-// text:
-// date:
-// postdetails:
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-
-    message: String,
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    date: Date
-})
+	message: String,
+	author: { type: Schema.Types.ObjectId, ref: 'User' },
+	date: Date
+});
 
 const Comment = mongoose.model('Comment', blogSchema);
 
--- GoodWatch/models/blog.js
+++ GoodWatch/models/blog.js
@@ -1,29 +1,15 @@
-// Ready for work
-
-// Models
-
-
-// Posts
-
-// tnterest:
-// text:
-// date:
-// comments:
-// author:
-
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    imageUrl: String,
-    interest: String,
-    title: String,
-    message: String,
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    comments: { type: Schema.Types.ObjectId, ref: 'Comment' },
-    date: Date
-})
+	imageUrl: String,
+	interest: String,
+	title: String,
+	message: String,
+	author: { type: Schema.Types.ObjectId, ref: 'User' },
+	comments: { type: Schema.Types.ObjectId, ref: 'Comment' },
+	date: Date
+});
 
 const Blog = mongoose.model('Blog', blogSchema);
 
