3a992f68402aa752097d373142487576b0b9cb65
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-19 17:27:59 -0400

    pulling

--- GoodWatch/models/User.js
+++ GoodWatch/models/User.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	username: String,
-	password: String,
-	email: String,
-	profilePic: String,
-	likes: Array,
-	theme: String,
-	blogURL: String,
-	interest: Array,
-	posts: { type: Schema.Types.ObjectId, ref: 'Blog' },
-	followers: [ { type: Schema.Types.ObjectId, ref: 'User' } ]
+    username: String,
+    password: String,
+    email: String,
+    profilePic: String,
+    likes: Array,
+    theme: String,
+    blogURL: String,
+    interest: Array,
+    posts: { type: Schema.Types.ObjectId, ref: 'Blog' },
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 
 const User = mongoose.model('User', userSchema);
