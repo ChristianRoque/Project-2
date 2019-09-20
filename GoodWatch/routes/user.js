33cd53a88d8ffbd1a24016e7f5a66ccba01dc669
Christian Roque <croqu008@fiu.edu>
2019-09-20 14:33:18 -0400

    this is a message

--- GoodWatch/routes/user.js
+++ GoodWatch/routes/user.js
@@ -6,138 +6,133 @@ const Blog = require('../models/blog');
 const passport = require('passport');
 const nodemailer = require('nodemailer');
 const templates = require('../templates/template');
-const uploadCloud = require('../config/cloudinary.js');
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% USER AUTH %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-    res.render('index');
+	res.render('index');
 });
 
-router.post('/signup', uploadCloud.single('profilePic'), (req, res, next) => {
-    let { email, subject, message } = req.body;
+router.post('/signup', (req, res, next) => {
+	let { email, subject, message } = req.body;
 
-    const imgPath = req.file.url;
-    const imgName = req.file.originalname;
+	const username = req.body.username;
+	const password = req.body.password;
+	const Email = req.body.email;
+	const profilePic = req.body.profilePic;
+	const theme = req.body.theme;
+	const blogurl = req.body.blogURL;
+	const coverPic = req.body.coverPic;
 
-    const username = req.body.username;
-    const password = req.body.password;
-    const Email = req.body.email;
-    const profilePic = req.body.profilePic;
-    const theme = req.body.theme;
-    const blogurl = req.body.blogURL;
-    const coverPic = req.body.coverPic;
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(password, salt);
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'views.blogpost@gmail.com',
-            pass: 'Ms134life'
-        }
-    });
-    User.create({
-        username: username,
-        password: hash,
-        emaill: email,
-        profilePic: imgPath,
-        theme: theme,
-        blogURL: blogurl,
-        coverPic: coverPic,
-        blogs: []
-    });
-    transporter
-        .sendMail({
-            from: '"VERIFY YOUR ACCOUNT" <views.blogpost@gmail.com>',
-            to: email,
-            subject: subject,
-            html: templates.templateExample(username)
-        })
-        .then(() => {
-            res.redirect('/login');
-        })
-        .catch((err) => {
-            next(err);
-        });
+	let transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'views.blogpost@gmail.com',
+			pass: 'Ms134life'
+		}
+	});
+	User.create({
+		username: username,
+		password: hash,
+		emaill: email,
+		profilePic: profilePic,
+		theme: theme,
+		blogURL: blogurl,
+		coverPic: coverPic,
+		blogs: []
+	});
+	transporter
+		.sendMail({
+			from: '"VERIFY YOUR ACCOUNT" <views.blogpost@gmail.com>',
+			to: email,
+			subject: subject,
+			html: templates.templateExample(username)
+		})
+		.then(() => {
+			res.redirect('/login');
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 router.get('/signup', (req, res, next) => {
-    res.render('users/signup');
+	res.render('users/signup');
 });
 
 ////////////////////////////////// LOGIN
 router.get('/login', (req, res, next) => {
-    res.render('users/login');
+	res.render('users/login');
 });
 
 router.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: '/profile',
-        failureRedirect: '/login',
-        failureFlash: true,
-        passReqToCallback: true
-    })
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true,
+		passReqToCallback: true
+	})
 );
 
 ///////////////////////////////////////// LOGOUT
 
 router.get('/logout', (req, res, next) => {
-    req.logOut();
-    res.redirect('/');
+	req.logOut();
+	res.redirect('/');
 });
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% USER PAGES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 router.get('/profile', (req, res, next) => {
-    User.findById(req.user._id).populate({ path: 'blogs', populate: { path: 'comments' } }).then((user) => {
-        console.log(user.blogs);
-        if (req.user) {
-            res.render('users/profile', { theUser: user });
-        } else {
-            res.redirect('/login');
-        }
-    });
+	User.findById(req.user._id).populate({ path: 'blogs', populate: { path: 'comments' } }).then((user) => {
+		console.log(user.blogs);
+		if (req.user) {
+			res.render('users/profile', { User: user });
+		} else {
+			res.redirect('/login');
+		}
+	});
 });
 
 router.get('/settings', (req, res, next) => {
-    res.render('users/edit-account');
+	res.render('users/edit-account');
 });
 
 router.post('/settings', (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const email = req.body.email;
-    const profilePic = req.body.profilePic;
-    const theme = req.body.theme;
-    const blogurl = req.body.blogURL;
-    const coverPic = req.body.coverPic;
+	const username = req.body.username;
+	const password = req.body.password;
+	const email = req.body.email;
+	const profilePic = req.body.profilePic;
+	const theme = req.body.theme;
+	const blogurl = req.body.blogURL;
+	const coverPic = req.body.coverPic;
 
-    User.findByIdAndUpdate(req.user._id, {
-        username: username,
-        email: email,
-        profilePic: profilePic,
-        theme: theme,
-        blogURL: blogurl,
-        coverPic: coverPic
-    }).then((well) => {
-        res.redirect('/profile');
-    });
+	User.findByIdAndUpdate(req.user._id, {
+		username: username,
+		email: email,
+		profilePic: profilePic,
+		theme: theme,
+		blogURL: blogurl,
+		coverPic: coverPic
+	}).then((well) => {
+		res.redirect('/profile');
+	});
 });
 
-router.get('/:userBlog', (req, res, next) => {
-    let key = req.params.userBlog;
-    User.findOne({ blogURL: key }).populate({ path: 'blogs', populate: { path: 'comments' } }).then((user) => {
-        console.log(user);
-        res.render('users/profile', { User: user });
-    });
+router.get('/profile/:userBlog', (req, res, next) => {
+	let key = req.params.userBlog;
+	User.findOne({ blogURL: key }).populate({ path: 'blogs', populate: { path: 'comments' } }).then((user) => {
+		res.render('users/profile', { User: user });
+	});
 });
 
 router.post('/delete-account', (req, res, next) => {
-    User.findByIdAndDelete(req.user);
+	User.findByIdAndDelete(req.user);
 });
 
 module.exports = router;
