9a4c5dc86fad6b721cafc77d6785c5c52434a57a
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-22 00:50:26 -0400

    login form final for user with accounts

--- GoodWatch/models/blog.js
+++ GoodWatch/models/blog.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-	imageUrl: String,
-	interest: String,
-	title: String,
-	message: String,
-	author: { type: Schema.Types.ObjectId, ref: 'User' },
-	comments: { type: Schema.Types.ObjectId, ref: 'Comment' },
-	date: Date
+    imageUrl: String,
+    interest: String,
+    title: String,
+    message: String,
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    date: Date
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
