894454cff1b8df55325f3dae63f940ddb3c61ba5
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-23 13:03:26 -0400

    blog routes

--- GoodWatch/models/blog.js
+++ GoodWatch/models/blog.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-	imageUrl: String,
-	interest: String,
-	title: String,
-	message: String,
-	author: { type: Schema.Types.ObjectId, ref: 'User' },
-	date: Date
+    imageUrl: String,
+    interest: String,
+    title: String,
+    message: String,
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    date: Date
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
--- GoodWatch/routes/blog.js
+++ GoodWatch/routes/blog.js
@@ -3,11 +3,126 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const passport = require('passport');
-const nodemailer = require('nodemailer');
-const templates = require('../templates/template');
+const Blog = require('../models/blog')
 
+// router.get('/blog', (req, res, next) => {
+//     res.render('blogs/new');
+// });
 router.get('/blog', (req, res, next) => {
-	res.render('blogs/new');
-});
+    Blog.find()
+        .then((allBlogs) => {
+            console.log(allBlogs)
+            res.render('blog/index', { blogs: allBlogs })
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+router.get('/blogs/details/:theid', (req, res, next) => {
+    let id = req.params.theid
+
+    Blog.findById(id).populate('author')
+        .then((blogObject) => {
+            console.log("AUTHOR", blogObject)
+            console.log(blogObject.author)
+            res.render('blogs/details', { theBlog: blogObject })
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+router.get('/blogs/new', (req, res, next) => {
+    Author.find()
+        .then((result) => {
+            res.render('blogs/new', { allTheauthor: result });
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
+router.post('/blog/creation', (req, res, next) => {
+
+    let imageUrl = req.body.theimageUrl;
+    let interest = req.body.theInterest
+    let title = req.body.theTitle;
+    let message = req.body.theMessage;
+    let author = req.body.theAuthor;
+    let date = req.body.theDate
+
+    Blog.create({
+            imgageUrl: imageUrl,
+            interest: interest,
+            title: title,
+            message: message,
+            author: author,
+            date: date
+        })
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+router.post('/blogs/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Blog.findByIdAndRemove(id)
+        .then((result) => {
+            res.redirect('/blog')
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
+router.get('/blogs/editblog/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Blog.findById(id)
+        .then((theBlog) => {
+            author.find()
+                .then((authorResult) => {
+                    data = {
+                        blog: theBlog,
+                        author: authorResult
+                    };
+                    res.render('blogs/edit', data);
+                })
+                .catch((err) => {
+                    next(err);
+                });
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
+router.post('/blogs/update/:id', (req, res, next) => {
+
+    let id = req.params.id;
+
+    Blog.findByIdAndUpdate(id, {
+
+            imageUrl: req.body.theimageUrl,
+            interest: req.body.theInterest,
+            title: req.body.theTitle,
+            message: req.body.theMessage,
+            author: req.body.theAuthor,
+            date: req.body.theDate
+
+        })
+        .then(() => {
+            res.redirect('/blogs/details/' + id)
+        })
+        .catch((err) => {
+            next(err);
+        })
+
+})
 
 module.exports = router;
