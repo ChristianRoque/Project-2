9b4a2fbb3c82186dadadd51cb3cfcb4bea4d2c1a
Christian Roque <croqu008@fiu.edu>
2019-09-23 14:05:13 -0400

    reworked routes

--- GoodWatch/models/blog.js
+++ GoodWatch/models/blog.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    imageUrl: String,
-    interest: String,
-    title: String,
-    message: String,
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    date: Date
+	imageURL: String,
+	interest: String,
+	title: String,
+	message: String,
+	author: String,
+	date: String
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
--- GoodWatch/routes/blog.js
+++ GoodWatch/routes/blog.js
@@ -3,126 +3,108 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const passport = require('passport');
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
 
 // router.get('/blog', (req, res, next) => {
 //     res.render('blogs/new');
 // });
-router.get('/blog', (req, res, next) => {
-    Blog.find()
-        .then((allBlogs) => {
-            console.log(allBlogs)
-            res.render('blog/index', { blogs: allBlogs })
-        })
-        .catch((err) => {
-            next(err);
-        })
-})
+router.get('/blogs', (req, res, next) => {
+	Blog.find()
+		.then((allBlogs) => {
+			console.log(allBlogs);
+			res.render('blogs/index', { blogs: allBlogs });
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
-router.get('/blogs/details/:theid', (req, res, next) => {
-    let id = req.params.theid
+router.get('/blogs/:theid', (req, res, next) => {
+	let id = req.params.theid;
 
-    Blog.findById(id).populate('author')
-        .then((blogObject) => {
-            console.log("AUTHOR", blogObject)
-            console.log(blogObject.author)
-            res.render('blogs/details', { theBlog: blogObject })
-        })
-        .catch((err) => {
-            next(err);
-        })
-})
+	Blog.findById(id)
+		.then((blogObject) => {
+			res.render('blogs/details', { theBlog: blogObject });
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
-router.get('/blogs/new', (req, res, next) => {
-    Author.find()
-        .then((result) => {
-            res.render('blogs/new', { allTheauthor: result });
-        })
-        .catch((err) => {
-            next(err)
-        })
-})
+router.get('/new-blog', (req, res, next) => {
+	res.render('blogs/new');
+	console.log(req.user);
+});
 
-router.post('/blog/creation', (req, res, next) => {
+router.post('/new-blog', (req, res, next) => {
+	let imageURL = req.body.imageURL;
+	let interest = req.body.interest;
+	let title = req.body.title;
+	let message = req.body.message;
+	let author = req.user.username;
 
-    let imageUrl = req.body.theimageUrl;
-    let interest = req.body.theInterest
-    let title = req.body.theTitle;
-    let message = req.body.theMessage;
-    let author = req.body.theAuthor;
-    let date = req.body.theDate
-
-    Blog.create({
-            imgageUrl: imageUrl,
-            interest: interest,
-            title: title,
-            message: message,
-            author: author,
-            date: date
-        })
-        .then(() => {
-            res.redirect('/')
-        })
-        .catch((err) => {
-            next(err);
-        })
-})
+	Blog.create({
+		imageURL: imageURL,
+		interest: interest,
+		title: title,
+		message: message,
+		author: author,
+		date: new Date()
+	})
+		.then(() => {
+			res.redirect('/');
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
 router.post('/blogs/delete/:id', (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    Blog.findByIdAndRemove(id)
-        .then((result) => {
-            res.redirect('/blog')
-        })
-        .catch((err) => {
-            next(err)
-        })
-})
+	Blog.findByIdAndRemove(id)
+		.then((result) => {
+			res.redirect('/blogs');
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
 router.get('/blogs/editblog/:id', (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    Blog.findById(id)
-        .then((theBlog) => {
-            author.find()
-                .then((authorResult) => {
-                    data = {
-                        blog: theBlog,
-                        author: authorResult
-                    };
-                    res.render('blogs/edit', data);
-                })
-                .catch((err) => {
-                    next(err);
-                });
-        })
-        .catch((err) => {
-            next(err)
-        })
-})
+	Blog.findById(id)
+		.then((theBlog) => {
+			res.render('blogs/edit', { blog: theBlog });
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
-router.post('/blogs/update/:id', (req, res, next) => {
+router.post('/blogs/editblog/:id', (req, res, next) => {
+	let id = req.params.id;
+	let imageURL = req.body.imageURL;
+	let interest = req.body.interest;
+	let title = req.body.title;
+	let message = req.body.message;
+	let author = req.user.username;
 
-    let id = req.params.id;
-
-    Blog.findByIdAndUpdate(id, {
-
-            imageUrl: req.body.theimageUrl,
-            interest: req.body.theInterest,
-            title: req.body.theTitle,
-            message: req.body.theMessage,
-            author: req.body.theAuthor,
-            date: req.body.theDate
-
-        })
-        .then(() => {
-            res.redirect('/blogs/details/' + id)
-        })
-        .catch((err) => {
-            next(err);
-        })
-
-})
+	Blog.findByIdAndUpdate(id, {
+		imageURL: imageURL,
+		interest: interest,
+		title: title,
+		message: message,
+		author: author,
+		date: new Date()
+	})
+		.then(() => {
+			res.redirect('/blogs');
+		})
+		.catch((err) => {
+			next(err);
+		});
+});
 
 module.exports = router;
