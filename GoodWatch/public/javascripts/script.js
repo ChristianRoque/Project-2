e42386d24b0b79331ec52c04086f50f000ac9042
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-24 12:07:47 -0400

    push

--- GoodWatch/public/javascripts/script.js
+++ GoodWatch/public/javascripts/script.js
@@ -14,17 +14,8 @@ function onClick(element) {
 var span = document.getElementById('close');
 var modal = document.getElementById('modal01');
 
-function onClick(element) {
-    modal.style.display = 'block';
-    document.getElementById('img01').src = element.src;
-    document.getElementsByClassName('modal-content').innerHTML = this.alt;
-}
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
 };
-
-function close() {
-    modal.style.display = 'none';
-}
