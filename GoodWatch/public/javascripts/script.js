497cf0063ecd6a7e30480b05c4ab0bf2c2df6bf2
Christian Roque <croqu008@fiu.edu>
2019-09-24 12:01:24 -0400

    pullinh

--- GoodWatch/public/javascripts/script.js
+++ GoodWatch/public/javascripts/script.js
@@ -1,23 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		console.log('IronGenerator JS imported successfully!');
+	},
+	false
+);
 
-    console.log('IronGenerator JS imported successfully!');
-
-}, false);
-
+var span = document.getElementById('close');
+var modal = document.getElementById('modal01');
 
 function onClick(element) {
-    document.getElementById("modal01").style.display = "block";
-    document.getElementById("img01").src = element.src;
-    document.getElementsByClassName("modal-content").innerHTML = this.alt;
+	modal.style.display = 'block';
+	document.getElementById('img01').src = element.src;
+	document.getElementsByClassName('modal-content').innerHTML = this.alt;
 }
 
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
 
-$('#exampleModal').on('show.bs.modal', function(event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
-})
+function close() {
+	modal.style.display = 'none';
+}
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+// 	modal.style.display = 'none';
+// };
+
+// When the user clicks anywhere outside of the modal, close it
+
+// // Get the modal
+
+// // Get the button that opens the modal
+// var btn = document.getElementById('myBtn');
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName('close')[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+// 	modal.style.display = 'block';
+// };
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+// 	modal.style.display = 'none';
+// };
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = 'none';
+// 	}
+// };
