c8ef29c4175769f1500cd319c719ec325e53505b
Christian Roque <croqu008@fiu.edu>
2019-09-25 08:35:38 -0400

    fixes

--- GoodWatch/public/javascripts/script.js
+++ GoodWatch/public/javascripts/script.js
@@ -1,21 +1,21 @@
 document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-        console.log('IronGenerator JS imported successfully!');
-    },
-    false
+	'DOMContentLoaded',
+	() => {
+		console.log('IronGenerator JS imported successfully!');
+	},
+	false
 );
 
 var span = document.getElementById('close');
 var modal = document.getElementById('modal01');
 
 function onClick(element) {
-    modal.style.display = 'block';
-    document.getElementById('img01').src = element.src;
-
+	modal.style.display = 'block';
+	document.getElementById('img01').src = element.src;
+	document.getElementsByClassName('modal-content').innerHTML = this.alt;
 }
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
 };
