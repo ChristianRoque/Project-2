30a481e741f327c07766d545bc5ec9ece93b3dc8
Christian Roque <croqu008@fiu.edu>
2019-09-25 11:05:46 -0400

    todays' second version

--- GoodWatch/public/javascripts/script.js
+++ GoodWatch/public/javascripts/script.js
@@ -6,18 +6,17 @@ document.addEventListener(
 	false
 );
 
-var span = document.getElementById('close');
-
 function onClick(element) {
-	console.log(element.alt);
 	var modal = document.getElementById(`modal${element.alt}`);
+
+	document.getElementById(`modal${element.alt}`).style.display = 'block';
+	document.getElementById('img01').src = element.src;
+
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = 'none';
 		}
 	};
-	document.getElementById(`modal${element.alt}`).style.display = 'block';
-	document.getElementById('img01').src = element.src;
 }
 
 // hello
