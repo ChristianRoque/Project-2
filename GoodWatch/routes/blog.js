5a98f034e26c5a5ac45acae7da41a5d2c397a5ee
Christian Roque <croqu008@fiu.edu>
2019-09-26 17:03:28 -0400

    trying to fix

--- GoodWatch/routes/blog.js
+++ GoodWatch/routes/blog.js
@@ -8,134 +8,109 @@ const Blog = require('../models/blog');
 const uploadCloud = require('../config/cloudinary.js');
 
 router.get('/blogs', (req, res, next) => {
-    Blog.find()
-        .then((allBlogs) => {
-            res.render('blogs/index', { blogs: allBlogs });
-        })
-        .catch((err) => {
-            next(err);
-        });
+	Blog.find()
+		.then((allBlogs) => {
+			res.render('blogs/index', { blogs: allBlogs });
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 router.get('/blogs/:id', (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    Blog.findById(id)
-        .populate('author')
-        .populate({ path: 'comments', populate: { path: 'author' } })
-        .then((blogObject) => {
-            res.render('blogs/details', { theBlog: blogObject });
-        })
-        .catch((err) => {
-            next(err);
-        });
+	Blog.findById(id)
+		.populate('author')
+		.populate({ path: 'comments', populate: { path: 'author' } })
+		.then((blogObject) => {
+			res.render('blogs/details', { theBlog: blogObject });
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 router.get('/new-blog', (req, res, next) => {
-    res.render('blogs/new');
+	res.render('blogs/new');
 });
 
 router.post('/new-blog', uploadCloud.single('blogphoto'), (req, res, next) => {
-    const imgPath = req.file.url;
-    let imageURL = req.body.imageURL;
-    let interest = req.body.interest;
-    let title = req.body.title;
-    let message = req.body.message;
-    let author = req.user.username;
+	const imgPath = req.file.url;
+	let imageURL = req.body.imageURL;
+	let interest = req.body.interest;
+	let title = req.body.title;
+	let message = req.body.message;
+	let author = req.user.username;
 
-    if (req.user) {
-        Blog.create({
-                imageURL: imgPath,
-                interest: interest,
-                title: title,
-                message: message,
-                author: req.user._id,
-                comments: [],
-                date: new Date()
-            })
-            .then((blog) => {
-                User.findByIdAndUpdate(req.user._id, { $push: { blogs: blog._id } }).then((user) => {});
-                res.redirect('/blogs');
-            })
-            .catch((err) => {
-                next(err);
-            });
-    } else {
-        res.redirect('/login');
-    }
+	if (req.user) {
+		Blog.create({
+			imageURL: imgPath,
+			interest: interest,
+			title: title,
+			message: message,
+			author: req.user._id,
+			comments: [],
+			date: new Date()
+		})
+			.then((blog) => {
+				User.findByIdAndUpdate(req.user._id, { $push: { blogs: blog._id } }).then((user) => {});
+				res.redirect('/blogs');
+			})
+			.catch((err) => {
+				next(err);
+			});
+	} else {
+		res.redirect('/login');
+	}
 });
 
 router.post('/blogs/delete/:id', (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    Blog.findByIdAndRemove(id)
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            next(err);
-        });
+	Blog.findByIdAndRemove(id)
+		.then((result) => {
+			res.redirect('/blogs');
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 router.get('/blogs/editblog/:id', (req, res, next) => {
-    let id = req.params.id;
+	let id = req.params.id;
 
-    if (req.user) {
-        Blog.findById(id)
-            .then((blog) => {
-                console.log(blog._id);
-                if (req.user._id.equals(blog.author)) {
-                    res.render('blogs/edit', { blog: blog });
-                } else {
-                    res.redirect(`/blogs/${blog._id}`);
-                }
-            })
-            .catch((err) => {
-                next(err);
-            });
-    } else {
-        res.redirect('/login');
-    }
+	if (req.user) {
+		Blog.findById(id)
+			.then((blog) => {
+				console.log(blog._id);
+				if (req.user._id.equals(blog.author)) {
+					res.render('blogs/edit', { blog: blog });
+				} else {
+					res.redirect(`/blogs/${blog._id}`);
+				}
+			})
+			.catch((err) => {
+				next(err);
+			});
+	} else {
+		res.redirect('/login');
+	}
 });
 
 router.post('/blogs/editblogs/:id', uploadCloud.single('blogphoto'), (req, res, next) => {
-    const imgPath = req.file.url;
-    let id = req.params.id;
-    let imageURL = req.body.imageURL;
-    let interest = req.body.interest;
-    let title = req.body.title;
-    let message = req.body.message;
+	const imgPath = req.file.url;
+	let id = req.params.id;
+	let imageURL = req.body.imageURL;
+	let interest = req.body.interest;
+	let title = req.body.title;
+	let message = req.body.message;
 
-<<<<<<< HEAD
-    if (req.user) {
-        Blog.findById(id).then((blog) => {
-            if (req.user._id.equals(blog.author)) {
-                Blog.findByIdAndUpdate(id, {
-                        imageURL: imgPath,
-                        interest: interest,
-                        title: title,
-                        message: message,
-                        date: new Date()
-                    })
-                    .then(() => {
-                        res.redirect('/blogs');
-                    })
-                    .catch((err) => {
-                        next(err);
-                    });
-            } else {
-                res.redirect('/login');
-            }
-        });
-    } else {
-        res.redirect('/login');
-    }
-=======
 	if (req.user) {
 		Blog.findById(id).then((blog) => {
 			if (req.user._id.equals(blog.author)) {
 				Blog.findByIdAndUpdate(id, {
-					imageURL: imageURL,
+					imageURL: imgPath,
 					interest: interest,
 					title: title,
 					message: message,
@@ -154,37 +129,36 @@ router.post('/blogs/editblogs/:id', uploadCloud.single('blogphoto'), (req, res, 
 	} else {
 		res.redirect('/login');
 	}
->>>>>>> 59900bf57162efffe3634f39816a8eae8d29346a
 });
 
 router.get('/myblogs', (req, res, next) => {
-    if (req.user) {
-        User.findById(req.user._id).populate('blogs').then((user) => {
-            if (req.user._id.equals(user._id)) {
-                res.render('blogs/myblogs', { User: user });
-            }
-        });
-    } else {
-        res.redirect('/login');
-    }
+	if (req.user) {
+		User.findById(req.user._id).populate('blogs').then((user) => {
+			if (req.user._id.equals(user._id)) {
+				res.render('blogs/myblogs', { User: user });
+			}
+		});
+	} else {
+		res.redirect('/login');
+	}
 });
 
 router.get('/myblog/:id', (req, res, next) => {
-    let id = req.params.id;
-    if (req.user) {
-        Blog.findById(id)
-            .populate('author')
-            .populate({ path: 'comments', populate: { path: 'author' } })
-            .then((blog) => {
-                if (req.user._id.equals(blog.author._id)) {
-                    res.render('blogs/mydetails', { theBlog: blog });
-                } else {
-                    res.redirect(`/blogs/${blog._id}`);
-                }
-            });
-    } else {
-        res.redirect('/login');
-    }
+	let id = req.params.id;
+	if (req.user) {
+		Blog.findById(id)
+			.populate('author')
+			.populate({ path: 'comments', populate: { path: 'author' } })
+			.then((blog) => {
+				if (req.user._id.equals(blog.author._id)) {
+					res.render('blogs/mydetails', { theBlog: blog });
+				} else {
+					res.redirect(`/blogs/${blog._id}`);
+				}
+			});
+	} else {
+		res.redirect('/login');
+	}
 });
 
 module.exports = router;
